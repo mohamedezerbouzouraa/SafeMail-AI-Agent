229cbab6166ea7d689c75192e6dd6aa1deee4ad1
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-24 22:27:57 +0100

    Implement session creation

--- services/session_service.py
+++ services/session_service.py
@@ -10,5 +10,4 @@ async def create_session():
     await session_service.create_session(
         app_name=APP_NAME,
         user_id=USER_ID,
-        session_id=SESSION_ID
-    )
+        session_id=SESSION_ID)

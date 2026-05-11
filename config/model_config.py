09204a64844626eaff436ccd0cb5d0015c3d4169
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-11 23:17:18 +0100

    Add model configuration for Generative AI

--- config/model_config.py
+++ config/model_config.py
@@ -8,7 +8,6 @@ AGENT_MODEL = "models/gemini-1.5-pro-latest"
 safety_settings = [
     {   "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
         "threshold": "BLOCK_LOW_AND_ABOVE",},]
-
 generate_content_config = GenerationConfig(
     temperature=0.3,
     max_output_tokens=800,
